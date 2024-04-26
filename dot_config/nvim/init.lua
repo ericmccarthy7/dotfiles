45b8e541b5fbaaf714c9bf2282c2f4f185647746
Eric McCarthy <ericmccarthy7@gmail.com>
2024-04-26 00:28:22 -0400

    mjs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -137,7 +137,7 @@ require("lazy").setup({
 			lspconfig.tsserver.setup {
 				filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" }
 			}
-			
+
 			lspconfig.astro.setup {}
 
 			-- vuejs
@@ -213,7 +213,7 @@ require("lazy").setup({
 					})
 
 					vim.api.nvim_create_autocmd("BufWritePre", {
-						pattern = { "*.rs", "*.lua", "*.js", "*.jsx", "*.ts", "*.tsx", "*.vue", "*.yaml", "*.yml", "*.json" },
+						pattern = { "*.rs", "*.lua", "*.js", "*.jsx", "*.ts", "*.tsx", "*.vue", "*.yaml", "*.yml", "*.json", ".mjs" },
 						callback = function()
 							vim.lsp.buf.format { async = false }
 						end
