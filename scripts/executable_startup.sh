76c327630326661ec4df14c658f80655e071bd84
Eric McCarthy <ericmccarthy7@gmail.com>
2025-08-13 11:19:23 -0400

    screensharing arch

--- scripts/executable_startup.sh
+++ scripts/executable_startup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# ~/.config/hypr/startup.sh
+
+wait_for_window() {
+    local class_name="$1"
+    while ! hyprctl clients -j | jq -e ".[] | select(.initialTitle == \"$class_name\")" >/dev/null; do
+        sleep 0.1
+    done
+}
 
 hyprctl dispatch workspace 2
 wezterm &
+wait_for_window "wezterm"
 
 hyprctl dispatch workspace 1
 firefox &
